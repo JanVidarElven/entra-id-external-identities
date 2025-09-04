694067adcb72c12f0fd0e7052ece70021794ae08
Jan Vidar Elven <jan.vidar.elven@evidi.com>
2025-09-04 10:37:12 +0200

    fix: Update redirectUri in authConfig.js to point to the correct production URL

--- prereq-app-environment/frontend/authConfig.js
+++ prereq-app-environment/frontend/authConfig.js
@@ -3,7 +3,8 @@ const msalConfig = {
       clientId: "dbe20fc6-2974-448c-bf40-129d8be80e2f",
       authority: "https://login.microsoftonline.com/104742fb-6225-439f-9540-60da5f0317dc",
       //redirectUri: "http://localhost:3000",
-      redirectUri: "https://proud-wave-0e3167903.2.azurestaticapps.net",
+      //redirectUri: "https://proud-wave-0e3167903.2.azurestaticapps.net",
+      redirectUri: "https://cis2025.elven.no"
     },
     cache: {
       cacheLocation: "sessionStorage", // This configures where your cache will be stored
